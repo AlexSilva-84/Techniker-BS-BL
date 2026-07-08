d1af90dc29918a63dc2ac698b9b2b3c34df5fc17
AlexSilva-84 <silvasuico@gmail.com>
2026-07-08 16:11:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+body{
+    font-family: Arial, sans-serif;
+    background:#f4f4f4;
+    text-align:center;
+    margin-top:100px;
+}
 
+h1{
+    color:#0066cc;
+}
+
+button{
+    padding:12px 20px;
+    background:#0066cc;
+    color:white;
+    border:none;
+    border-radius:8px;
+    cursor:pointer;
+}
